1cdae8d9ee5ee1baca7effb8678ca292a5ad0e24
Rayanne B. Lima <rayanne_lima2010@hotmail.com>
2020-07-07 20:45:18 -0300

    Slider nas Imagens + Algumas Correções

---
+++ js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    $('.produtos').slick({
+        infinite: true,
+        speed: 1000,
+        autoplay: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.feedback-container').slick({
+        dots: true,
+        centerMode: true,
+        autoplay: true,
+        centerPadding: '80px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+
+
+});
