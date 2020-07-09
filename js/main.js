780281661114efae58f291aee17fb07cee5908f2
Rayanne B. Lima <rayanne_lima2010@hotmail.com>
2020-07-09 19:18:52 -0300

    Melhorias + Testes na Responsividade

--- js/main.js
+++ js/main.js
@@ -1,64 +1,73 @@
 $(document).ready(function () {
-    $('.produtos').slick({
-        infinite: true,
-        speed: 1000,
-        autoplay: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
+  // AUTO PLAY VIDEO
+  $('.thumb').click(function() {
+    $(".videoautoplay").attr('src', $(".videoautoplay").attr('src') + '?autoplay=1'); 
+  });
 
-    $('.feedback-container').slick({
-        dots: true,
-        centerMode: true,
-        autoplay: true,
-        centerPadding: '80px',
-        slidesToShow: 3,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          }
-        ]
-      });
+  // IMAGE SLIDER
+  $('.produtos').slick({
+    infinite: true,
+    speed: 1000,
+    autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 790,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('.feedback-container').slick({
+    dots: true,
+    centerMode: true,
+    autoplay: true,
+    centerPadding: '80px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 790,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      /*{
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }*/
+    ]
+  });
+
 
 
 });
+
+
